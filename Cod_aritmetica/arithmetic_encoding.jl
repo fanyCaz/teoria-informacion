b53c8b37d4f5fe521de537878273c56b22cbb7ae
fanyCaz <stepheniecaz@gmail.com>
2022-05-08 14:50:24 -0500

    got middle values

--- Cod_aritmetica/arithmetic_encoding.jl
+++ Cod_aritmetica/arithmetic_encoding.jl
@@ -1,29 +1,40 @@
-import numpy as np
-import math
+#import numpy as np
+#import math
 
-def getMiddleValues(letters, position, first_value: bool= False):
-  previus_lengths = math.prod(fi[:position-1]) if position-1 > len(fi) else 0
-  print(previus_lengths)
-  length = fi[position]
-  if first_value:
-    alpha = fi[position-1]+(fi[position-1]*previus_lengths)
-  beta = length + alpha
-  return alpha,beta,length
+function collected_values(values)
+  return sort(collect(keys(values)))
+end
+
+function getMiddleValues(position, pre_alpha, pre_len, first=false)
+  previus_lengths = first ? 0 : prod(fi[1:position])
+  len = fi[position]
+  alpha = fi[position-1] + fi[position-1]*previus_lengths
+  beta = len + alpha
+  return alpha,beta,len
+end
 
 fi = [0.25,0.2,0.2,0.15,0.1,0.1]
 
-alphabet = np.array(['a','b','c','d','e','f'])
+alphabet = ['a','b','c','d','e','f']
 
-sequence = 'bfac'
+sequence = "bfac"
+frequencies = Dict([ (letter,f) for (letter,f) in zip(alphabet,fi)])
+sorted_alphabet = collected_values(frequencies)
 
-first_letter = sequence[0]
-position = np.argwhere(alphabet == first_letter).flatten()[0]
-print(position)
-if position == 0:
-  alpha = fi[0]
-elif position == len(fi) - 1:
+first_letter = sequence[1]
+position = findfirst(x-> x == first_letter, sorted_alphabet)
+if position == 1
+  alpha = fi[1]
+elseif position == length(fi)
   alpha = 0
-else:
-  alpha,beta,length = getMiddleValues(first_letter,position,True)
+else
+  alpha,beta,len = getMiddleValues(position,frequencies[sorted_alphabet[position-1]], frequencies[sorted_alphabet[position-1]],true)
+end
+println("a $alpha b $beta, l $len")
 
-second_letter = sequence[1]
+second_letter = sequence[2]
+position = findfirst(x-> x == second_letter, sorted_alphabet) 
+println(position)
+
+
+
