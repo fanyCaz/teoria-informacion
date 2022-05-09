e418a67dff91c8ac12b81979f3dc1ba8e6da51d4
fanyCaz <stepheniecaz@gmail.com>
2022-05-09 12:06:09 -0500

    second attempt and working good, but theres a todo

--- Cod_aritmetica/arithmetic_encoding.jl
+++ Cod_aritmetica/arithmetic_encoding.jl
@@ -2,17 +2,20 @@ function collected_values(values)
   return sort(collect(keys(values)))
 end
 
-function getFirstIndexValues(prev_alpha,letters)
+function productLetterFrequencies(frequencies, letters)
+  return prod([ frequencies[letter] for letter in letters ])
+end
+
+function getFirstIndexValues(prev_alpha, frequencies, letters)
   alpha = prev_alpha
-  len = prod([ frequencies[letter] for letter in letters])
+  len = productLetterFrequencies(frequencies, letters)
   beta = alpha + len
   return alpha,beta,len
 end
 
-function getMiddleValues(position, pre_alpha, pre_len, first=false)
-  previus_lengths = first ? 0 : prod(fi[1:position])
-  len = fi[position]
-  println("posss $position len $len")
+function getMiddleValues(position, pre_alpha, pre_len, frequencies, letters, prev_letters, first=false)
+  previus_lengths = first ? 0 : productLetterFrequencies(frequencies, prev_letters)
+  len = productLetterFrequencies(frequencies, letters)
   alpha = pre_alpha + pre_len*previus_lengths
   beta = len + alpha
   return alpha,beta,len
@@ -20,77 +23,42 @@ end
 
 function getLastIndexValues(pre_beta,letters, frequencies)
   beta = pre_beta
-  len = prod([ frequencies[letter] for letter in letters])
+  len = productLetterFrequencies(frequencies, letters)
   alpha = beta - len
   return alpha,beta,len
 end
 
-fi = [0.25,0.2,0.2,0.15,0.1,0.1]
+function main()
+  fi = [0.25,0.2,0.2,0.15,0.1,0.1]
+  alphabet = ['a','b','c','d','e','f']
+  sequence = "bcda"
+  frequencies = Dict([ (letter,f) for (letter,f) in zip(alphabet,fi)])
+  sorted_alphabet = collected_values(frequencies)
+  println("FALTA DEMOSTRAR CUANDO ES LA PRIMERA LETRA")
 
-alphabet = ['a','b','c','d','e','f']
-
-sequence = "bfac"
-frequencies = Dict([ (letter,f) for (letter,f) in zip(alphabet,fi)])
-sorted_alphabet = collected_values(frequencies)
-
-first_letter = sequence[1]
-position = findfirst(x-> x == first_letter, sorted_alphabet)
-if position == 1
-  alpha = fi[1]
-elseif position == length(fi)
+  println("Inicio de ciclo")
+  first = true
   alpha = 0
-else
-  alpha,beta,len = getMiddleValues(position,frequencies[sorted_alphabet[position-1]], frequencies[sorted_alphabet[position-1]],true)
-end
-println("a $alpha b $beta, l $len")
-
-second_letter = sequence[2]
-position = findfirst(x-> x == second_letter, sorted_alphabet) 
-println(position)
-
-if position == 1
-  alpha = fi[1]
-elseif position == length(fi)
-  println("entro aqui")
-  # sequence[valor de index de palabra en sequecia]
-  alpha,beta,len = getLastIndexValues(beta,[sequence[2-1],second_letter],frequencies)
-else
-  alpha,beta,len = getMiddleValues(position,frequencies[sorted_alphabet[position-1]], frequencies[sorted_alphabet[position-1]],true)
-end
-println("a $alpha b $beta, l $len")
-
-third_letter = sequence[3]
-position = findfirst(x-> x == third_letter, sorted_alphabet) 
-
-if position == 1
-  println("position 1 entro aqui")
-  alpha,beta,len = getFirstIndexValues(alpha,sequence[1:3])
-elseif position == length(fi)
-  alpha,beta,len = getLastIndexValues(beta,[sequence[2-1],second_letter],frequencies)
-else
-  alpha,beta,len = getMiddleValues(position,frequencies[sorted_alphabet[position-1]], frequencies[sorted_alphabet[position-1]],true)
-end
-
-println("a $alpha b $beta, l $len")
-
-
-println("Inicio de ciclo")
-global first = true
-#global alpha, betha, long
-for (index,letter) in enumerate(sequence)
-  position_letter = findfirst(x-> x == letter, sorted_alphabet) 
-  if position_letter == 1
-    alpa,betha,long = getFirstIndexValues(alpa,sequence[1:index])
-  elseif position == length(fi)
-    alpa,betha,long = getLastIndexValues(betha,sequence[1:index],frequencies)
-  else
-    println("freq $(frequencies[sorted_alphabet[position_letter-1]]) ")
-    var =frequencies[sorted_alphabet[position_letter-1]] 
-    alpa,betha,long = getMiddleValues(position,var, var,first)
-    global first = false
+  betha = 0
+  long = 0
+  for (index,letter) in enumerate(sequence)
+    println("letra $letter")
+    position_letter = findfirst(x-> x == letter, sorted_alphabet) 
+    if position_letter == 1
+      alpha,betha,long = getFirstIndexValues(alpha,frequencies,sequence[1:index])
+    elseif position_letter == length(fi)
+      alpha,betha,long = getLastIndexValues(betha,sequence[1:index],frequencies)
+    else
+      var =frequencies[sorted_alphabet[position_letter-1]] 
+      prev_letters = sorted_alphabet[1:findfirst(isequal(letter), sorted_alphabet)-1]
+      pre_a, pre_l = first ? (var,var) : (alpha,long)
+      alpha,betha,long = getMiddleValues(position, pre_a, pre_l, frequencies, sequence[1:index], prev_letters, first)
+      first = false
+    end
+    println("a $alpha b $betha, l $long")
   end
-  println("a $alpa b $beta, l $long")
-  alpa,beta,long
+  alpha,betha,long
 end
 
+main()
 
