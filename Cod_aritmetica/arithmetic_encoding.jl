46d25a1004f9bc99f7d9cd3e6b30e7059615f4ce
fanyCaz <stepheniecaz@gmail.com>
2022-05-16 17:36:55 -0500

    adaptative but only beggining

--- Cod_aritmetica/arithmetic_encoding.jl
+++ Cod_aritmetica/arithmetic_encoding.jl
@@ -35,13 +35,7 @@ function getLastIndexValues(pre_beta,letters, frequencies)
   return alpha,beta,len
 end
 
-function main()
-  fi = [0.25,0.2,0.2,0.15,0.1,0.1]
-  alphabet = ['a','b','c','d','e','f']
-  sequence = "bfac"
-  frequencies = Dict([ (letter,f) for (letter,f) in zip(alphabet,fi)])
-  sorted_alphabet = collected_values(frequencies)
-  println("Inicio de ciclo")
+function arithmetic_encoding(frequencies, sorted_alphabet, sequence)
   first = true
   alpha = 0
   betha = 0
@@ -52,7 +46,7 @@ function main()
     position_letter = findfirst(x-> x == letter, sorted_alphabet) 
     if position_letter == 1
       alpha,betha,long = getFirstIndexValues(alpha,frequencies,sequence[1:index])
-    elseif position_letter == length(fi)
+    elseif position_letter == length(frequencies)
       betha = first ? frequencies[sorted_alphabet[position_letter]] : betha
       alpha,betha,long = getLastIndexValues(betha,sequence[1:index],frequencies)
     else
@@ -64,6 +58,44 @@ function main()
     first = false
   end
   println("a $alpha b $betha, l $long")
+  return alpha,betha,long
+end
+
+function arithmetic_adaptative(frequencies, sorted_alphabet, sequence)
+  first = true
+  alpha = 0
+  betha = 0
+  long = 0
+  # prev_letters = sorted letters, takes the previous of the current letter, if 'c' then takes 'a','b' and so on
+  for (index,letter) in enumerate(sequence)
+    println("letra $letter")
+    position_letter = findfirst(x-> x == letter, sorted_alphabet) 
+    if position_letter == 1
+      #alpha,betha,long = getFirstIndexValues(alpha,frequencies,sequence[1:index])
+    elseif position_letter == length(frequencies)
+      #betha = first ? frequencies[sorted_alphabet[position_letter]] : betha
+      #alpha,betha,long = getLastIndexValues(betha,sequence[1:index],frequencies)
+    else
+      #var =frequencies[sorted_alphabet[position_letter-1]]
+      #prev_letters = sorted_alphabet[1:findfirst(isequal(letter), sorted_alphabet)-1]
+      #pre_a, pre_l = first ? (var,var) : (alpha,long)
+      #alpha,betha,long = getMiddleValues(position, pre_a, pre_l, frequencies, sequence[1:index], prev_letters, first)
+    end
+    first = false
+  end
+  println("a $alpha b $betha, l $long")
+  return alpha,betha,long
+end
+
+function main()
+  fi = [0.25,0.2,0.2,0.15,0.1,0.1]
+  alphabet = ['a','b','c','d','e','f']
+  sequence = "bfac"
+  frequencies = Dict([ (letter,f) for (letter,f) in zip(alphabet,fi)])
+  sorted_alphabet = collected_values(frequencies)
+  println("Inicio de ciclo")
+  
+  alpha,betha,long = arithmetic_encoding(frequencies, sorted_alphabet, sequence)
   run_method_one(alpha,betha,long)
   run_method_two(alpha,betha)
 end
