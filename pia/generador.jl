3ae6befb5a408656f071cdad6de2f77c9c8f7778
Eduardo Salazar <eduardosalaz@outlook.com>
2022-05-22 13:31:07 -0500

    ascii -> utf8

--- pia/generador.jl
+++ pia/generador.jl
@@ -31,9 +31,11 @@ function generate()
     combinaciones = []
     symbols = []
     symbolized_conf = []
-    start_symbol = 'A' # ascii codepoint de 0x00000041, esto nos servirá más adelante
-    start_codepoint = codepoint('A')
-    bracket_codepoints = [codepoint('['), codepoint(']')]
+    # start_symbol = 'A' # ascii codepoint de 0x00000041, esto nos servirá más adelante
+    start_codepoint = 0x0021 # empieza en !
+    start_symbol = Char(start_codepoint)
+    not_allowed_codepoints = [0x005B, 0x005D, 0x002C, 0x0022]
+    number_codepoints = [codepoint(string(num)[1]) for num in 0:9] # no podemos usar numeros como simbolos porque se rompe lo demas
     # el codepoint de la Z es 0x0000005a
     # el codepoint de 'a' es 0x00000061
     # quizas empezar en 0x21? el codepoint del !
@@ -48,14 +50,18 @@ function generate()
             push!(combinaciones, digits)
             push!(symbols, start_symbol)
             push!(symbolized_conf, start_symbol)
-            if start_codepoint == 0x0000005a # los chars de enmedio no son simbolos validos
-                start_codepoint = 0x00000061
-            else
+            # if start_codepoint == 0x0000005a # los chars de enmedio no son simbolos validos
+            #    start_codepoint = 0x00000061
+            #else
                 start_codepoint += 1
-                if start_codepoint in bracket_codepoints
+                if start_codepoint in not_allowed_codepoints
                     start_codepoint +=1
                 end
-            end
+
+                if start_codepoint in number_codepoints
+                    start_codepoint = number_codepoints[end] + 1 # skip todo?
+                end
+            # end
             start_symbol = Char(start_codepoint)
         else
             for i in 1:length(combinaciones)
